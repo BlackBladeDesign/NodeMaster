8f650e7447cf7c7997ee9c20c77d3bc30793ed1c
BlackBladeDesign <darrenarpope@gmail.com>
2023-10-10 14:28:38 +1100

    Expanded node EXP to handle groups need to fix IMP

--- Operators/nm_nodeEXP.py
+++ Operators/nm_nodeEXP.py
@@ -2,7 +2,7 @@ import bpy
 import json
 from json import JSONEncoder
 from bpy_extras.io_utils import ExportHelper, ImportHelper
-
+from ..Operators.nm_operators import (connectNodes,createNode)
 
 class NodeEncoder(JSONEncoder):
     def default(self, obj):
@@ -16,40 +16,90 @@ class NodeEncoder(JSONEncoder):
         return super().default(obj)
 
 
-def export_node_tree(node_tree, file_path):
-    data = {'nodes': [], 'links': []}
+def assign_unique_ids(node_tree):
+    # Create a dictionary to store IDs for nodes and groups
+    id_dict = {}
+    current_id = 0
 
     for node in node_tree.nodes:
-        node_data = {
-            'name': node.name,
-            'label': node.label,
-            'type': type(node).__name__,
-            'location': (node.location.x, node.location.y),
-            'inputs': [(input.identifier, input.name, input.type)
-                       for input in node.inputs],
-            'outputs': [(output.identifier, output.name, output.type)
-                        for output in node.outputs]
-        }
-        if isinstance(node, bpy.types.ShaderNodeTexImage) and node.image is not None:
-            node_data['color_space'] = node.image.colorspace_settings.name
-        data['nodes'].append(node_data)
+        # Assign an ID to each node
+        id_dict[node.name] = current_id
+        current_id += 1
 
-    for link in node_tree.links:
-        data['links'].append((link.from_node, link.from_socket,
-                              link.to_node, link.to_socket))
+    for node in node_tree.nodes:
+        if node.type == 'GROUP':
+            # If it's a group, assign an ID to the group
+            id_dict[node.node_tree.name] = current_id
+            current_id += 1
+
+    return id_dict
+
+def export_node_tree(node_tree, file_path):
+    def export_node_tree_internal(node_tree,id_dict):
+        data = {'nodes': [], 'links': []}
+
+        node_dict = {}
+        for node in node_tree.nodes:
+            node_data = {
+                'name': node.name,
+                'label': node.label,
+                'type': type(node).__name__,
+                'location': (node.location.x, node.location.y),
+                'inputs': [],
+                'outputs': []
+            }
+            
+            if isinstance(node, bpy.types.ShaderNodeTexImage) and node.image is not None:
+                node_data['color_space'] = node.image.colorspace_settings.name
+                
+            if node.type == 'GROUP':
+                # Handle group inputs and outputs separately
+                node_data['name'] = node.node_tree.name
+
+                for input_idx, input_socket in enumerate(node.inputs):
+                    # Mark this input as active
+                    node_data['inputs'].append((input_idx, input_socket.name, input_socket.type))
+
+                for output_idx, output_socket in enumerate(node.outputs):
+                    # Mark this output as active
+                    node_data['outputs'].append((output_idx, output_socket.name, output_socket.type))
+                
+                # Recursively export nodes inside the group
+                node_data['nodes'] = export_node_tree_internal(node.node_tree, id_dict)
+            else:
+                # Regular node inputs and outputs
+                node_data['inputs'] = [(i, input.name, input.type) for i, input in enumerate(node.inputs)]
+                node_data['outputs'] = [(i, output.name, output.type) for i, output in enumerate(node.outputs)]
+
+
+
+            data['nodes'].append(node_data)
+            node_dict[node.name] = node
+
+        for link in node_tree.links:
+            from_node = link.from_node
+            from_socket = link.from_socket
+            to_node = link.to_node
+            to_socket = link.to_socket
+
+            from_socket_idx = list(from_node.outputs).index(from_socket)
+            to_socket_idx = list(to_node.inputs).index(to_socket)
+
+            data['links'].append((from_node.name, from_socket_idx, to_node.name, to_socket_idx))
+
+        return data
+
+    id_dict = assign_unique_ids(node_tree)  # Dictionary to store unique IDs for nodes and groups
+    data = export_node_tree_internal(node_tree, id_dict)
 
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, cls=NodeEncoder, ensure_ascii=False, indent=4)
 
     return {'FINISHED'}
 
-def import_node_tree(file_path):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
 
-    selected_material = bpy.context.active_object.active_material
-    node_tree = selected_material.node_tree
 
+def import_node_tree_internal(node_tree, data):
     node_dict = {}
     for node_data in data['nodes']:
         node_type = node_data['type']
@@ -60,20 +110,53 @@ def import_node_tree(file_path):
         node.location = node_location
         node.name = node_name
         node.label = node_label
-
+        
         if node_type == 'ShaderNodeTexImage':
             color_space = node_data.get('color_space')
             if color_space:
                 node.image.colorspace_settings.name = color_space
 
         node_dict[node_data['name']] = node
+        if node_type == 'ShaderNodeGroup':
+            groupNodes = node_data['nodes']
+            newGroup = bpy.data.node_groups.new(name=node.name, type='ShaderNodeTree')
+            for groupInput in node_data['inputs']:
+                input_name, input_type = groupInput[1], groupInput[2]
+                try:
+                    new_input = newGroup.inputs.new(input_type, input_name)
+                except RuntimeError as e:
+                    print(f"Error creating input socket for {input_name}: {e}")
+
+            for groupOutput in node_data['outputs']:
+                output_name, output_type = groupOutput[1], groupOutput[2]
+                try:
+                    new_output = newGroup.outputs.new(output_type, output_name)
+                except RuntimeError as e:
+                    print(f"Error creating output socket for {output_name}: {e}")
+
+            import_node_tree_internal(newGroup, groupNodes)
 
     for link_data in data['links']:
-        from_node = node_dict[link_data[0]]
-        from_socket = from_node.outputs[link_data[1]]
-        to_node = node_dict[link_data[2]]
-        to_socket = to_node.inputs[link_data[3]]
-        node_tree.links.new(from_socket, to_socket)
+        from_node = node_dict.get(link_data[0])
+        to_node = node_dict.get(link_data[2])
+
+        if from_node is not None and to_node is not None:
+            try:
+                from_socket = from_node.outputs[link_data[1]]
+                to_socket = to_node.inputs[link_data[3]]
+                node_tree.links.new(from_socket, to_socket)
+            except (IndexError, RuntimeError) as e:
+                print(f"Error creating link: {e}")
+
+    return {'FINISHED'}
+
+def import_node_tree(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    selected_material = bpy.context.active_object.active_material
+    node_tree = selected_material.node_tree
+    import_node_tree_internal(node_tree, data)
 
     return {'FINISHED'}
 
