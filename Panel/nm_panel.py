7abfe5d51cc01aad082547b5a083925457d88c94
Darren Pope <126746830+BlackBladeDesign@users.noreply.github.com>
2023-11-09 15:15:15 +1100

    Update nm_panel.py
    Removed deprecated mat settings and suffixes (Suffixes will be editable within the panel soon)

--- Panel/nm_panel.py
+++ Panel/nm_panel.py
@@ -2,7 +2,7 @@ import bpy
 from bpy.types import Panel
 from bpy.props import EnumProperty, StringProperty
 
-from ..Operators.nm_operators import (LoadFromPath, AutoLoad,AddProperty)
+from ..Operators.nm_operators import (LoadFromPath, AutoLoad,AddProperty, ExportTransforms)
 from ..Props.nm_props import NodeMasterProperties
 
 
@@ -66,46 +66,6 @@ class fileSettingsPanel(bpy.types.Panel):
         row.prop(context.scene.nm_props, "texturePath", text="Texture Path")
         row.prop(context.scene.nm_props, "image_file_type", text=" File Type")
 
-        
-
-
-        
-class matSettingsPanel(bpy.types.Panel):
-    bl_label = "Material Settings"
-    bl_idname = "NODE_PT_nodemaster_matSettings"
-    bl_category = "NodeMaster"
-    bl_parent_id = "NODE_PT_nodemaster"
-    bl_region_type = 'UI'
-    bl_space_type = 'NODE_EDITOR'
-
-    def draw(self, context):
-        # Sub-panel for material settings
-        layout = self.layout
-
-        # Main buttons and path
-        row = layout.column()
-        row.prop(context.scene.nm_props, "gltf_Node", text="GLTF/GLB Output")
-        row.prop(context.scene.nm_props, "displacement", text="Displacment")
-        row.prop(context.scene.nm_props, "texCoord", text="Tex Co-ord")
-
-
-class texSuffixPanel(bpy.types.Panel):        
-    bl_label = "Texture Suffixes"
-    bl_idname = "NODE_PT_nodemaster_texSuffix"
-    bl_category = "NodeMaster"
-    bl_parent_id = "NODE_PT_nodemaster"
-    bl_region_type = 'UI'
-    bl_space_type = 'NODE_EDITOR'
-
-    def draw(self, context):
-        layout = self.layout
-        # Main buttons and path
-        row2 = layout.column()
-        row2.prop(context.scene.nm_props, "normal_map", text="- Normal Map")
-        row2.prop(context.scene.nm_props, "base_color", text="- Base Color")
-        row2.prop(context.scene.nm_props, "orm_texture", text="- ORM")
-        row2.prop(context.scene.nm_props, "roughness_texture", text="- Roughness")
-        row2.prop(context.scene.nm_props, "metallic_texture", text="- Metallic")
 class nmToolsPanel(bpy.types.Panel):        
     bl_label = "Tools"
     bl_idname = "NODE_PT_nodemaster_tools"
@@ -122,9 +82,10 @@ class nmToolsPanel(bpy.types.Panel):
         row.operator("node.imgcleaner", text="Clean Duplicate Images")
         row.operator("node.importjson", text="Load Node Tree (JSON)")
         row.operator("node.exportjson", text="Export Node Tree (JSON)")
-        row.operator("node.addproperty", text="Add Custom Property")
 
+        row.operator("node.addproperty", text="Add Custom Property")
         row.prop(context.scene.nm_props, "apply_propertyTo", text=" Apply Property To")
         row.prop(context.scene.nm_props, "customProperty", text="Custom Property")
         row.prop(context.scene.nm_props, "custom_property_val", text="Custom Property Value")
-
+        
+        row.operator("node.exporttransforms", text="Export Transforms for Babylon.js")
